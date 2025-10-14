9074e9327865e3c6915e179bf19b3e21daf4ee77
Jojo2525 <unverzagtjoern@yahoo.com>
2025-10-14 15:32:25 +0200

    fix: link bugs and style

--- scripts/background.ts
+++ scripts/background.ts
@@ -1,7 +1,10 @@
-// scripts/background.js
+// scripts/background.ts
 
 // Dieser Listener wird ausgeführt, wenn auf das Action-Icon der Erweiterung geklickt wird.
-chrome.action.onClicked.addListener((tab) => {
-  // Öffnet das Side Panel im aktuellen Fenster.
-  chrome.sidePanel.open({ windowId: tab.windowId });
+chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => {
+  // Wir prüfen sicherheitshalber, ob das Tab eine ID hat.
+  if (tab.windowId) {
+    // Öffnet das Side Panel im aktuellen Fenster.
+    chrome.sidePanel.open({ windowId: tab.windowId });
+  }
 });
